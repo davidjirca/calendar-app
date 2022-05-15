c25a5a81049b0ebf88d2e5770439394558f1edbe
davidjirca <davidgjirca@gmail.com>
2022-05-16 00:25:16 +0300

    init version

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+      <div class="w-auto flex">
+        <Calendar />
+      </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from "./components/Calendar";
+import Header from "./components/Header";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  // eslint-disable-next-line vue/no-unused-components
+  components: { Calendar, Header },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="w-full bg-gradient-to-r from-blue-500 to-blue-600 text-white px-4 py-2">
+    <a>Heroes</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
